4cecf8792ce3046d389b8726c604a4ad6c9a0caa
jerodtjohnson-tech <jerodtjohnson@gmail.com>
2026-09-08 16:38:12 -0500

    Add files via upload

---
+++ GCVB_Fall26_Programmatic_Display_set-1/300x250/js/ivd_mock_feed.js
@@ -0,0 +1,22 @@
+var ivd_mock_feed = {
+  Publisher: "Trade Desk",
+  Publisher_ID: "",
+  creative: "normal ",
+  test: "Launch 1 is a go",
+  another_test:"Launch 2 is a go",
+  name: "Jeremiah Johnson",
+  cta_text: "Unlock your offer",
+  headline_1: "Built for Wisconsin.",
+  subhead_1: "Chequamegon? Manitowoc? Kaukauna? Weyauwega? Yup.",
+  headline_2: "US Cellular customers,",
+  subhead_2: "join a Wisconsin company with coverage far and wide.",
+  headline_3: "Switch Today",
+  subhead_3: "Keep your number and phone and get 6 months free service. And we'll buyout your contract.",
+  disclaimer: "Legal Text",
+  offer_image: "blank",
+  background:"bg-c.jpg",
+  clickthroughUrl: "https://www.mscdirect.com/",
+  /////////////////////////////////
+};
+
+
